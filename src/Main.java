0b5233bf98881dd2870b8bdfc695181a6959b389
MARA <cmarateodora@gmail.com>
2023-05-06 13:48:06 +0300

    created logic for repetitive letter input

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
@@ -10,7 +11,9 @@ public class Main {
         // 5. parcurgem cuv si comparam cu fiecare litera din cuv cu litera salvata
         // 6.daca exista litera inlocuim steluta de la pozitia literei din cuvant in sirul de stelute
 
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Hangman!");
+
         System.out.print("Cuvantul este ");
         String hiddenWord = "avion";
         char[] stars = new char[hiddenWord.length()];
@@ -20,6 +23,27 @@ public class Main {
 //        }
         Arrays.fill(stars, '*');
         System.out.println(String.valueOf(stars));
+
+        while (true){
+        System.out.println("Introdu o litera:");
+        char letter = sc.next().charAt(0);
+        for (int i = 0; i < hiddenWord.length(); i++)
+        {
+            if(hiddenWord.charAt(i) == letter)
+            {
+                stars[i] = letter;
+            }
+
+        }
+            System.out.println(String.valueOf(stars));
+
+        if(hiddenWord .equals(String.valueOf(stars)))
+        {
+            System.out.println("Felicitari! Ai castigat.");
+            break;
+        }
+        }
+
 
 
 
